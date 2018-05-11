2db3ecf61ab999714842ae1ac4a1d85bf4b606a1
Francomitoire <francomitoire@gmail.com>
2018-05-11 14:25:58 -0300

    mas o menos corre ahora xd cruza parametrizada y correcta seleccion parametrizada y correcta esquema de algoritmo genetico generico implementado

--- Clusters/ultimaTormenta.py
+++ Clusters/ultimaTormenta.py
@@ -2,6 +2,8 @@ import math
 import matplotlib.pyplot as pl
 import random
 import numpy as np
+from copy import copy
+import warnings
 
 
 class Individuo(object):
@@ -28,9 +30,9 @@ class Individuo(object):
         self.valor = individuo
         self.setPuntos(dataset)
 
-    def setValor(self,valor,dataset):
+    def setValor(self,valor):
         self.valor = valor
-        self.setPuntos(dataset)
+
 
     def setFitness(self):
         #arreglar fitnes, colocar fitness = 99999 y colocar los if si no procesa ningun dato que quede esa fitness
@@ -60,7 +62,7 @@ class Individuo(object):
 
         # if fitness > Individuo.mayorFitness:
         #     Individuo.mayorFitness = round(fitness,2)
-        self.fitness = 1000 - round(fitness,2)
+        self.fitness = 2000 - round(fitness,2)
         #print('Fitness: ' + str(fitness))
 
     # def corregirFitness(self):
@@ -145,9 +147,9 @@ def seleccionControlada(pob):
         cant= int(c[i])
         copias.append(int(cant))
         for j in range(cant):
-            indiv = Individuo()
             indiv = poblacion[i]
             pob_salida.append(indiv)
+
 
     while sum(copias) !=  len(poblacion):
 
@@ -156,83 +158,119 @@ def seleccionControlada(pob):
             copia = 0
             if random.uniform(0,1) <= probabilidad:
                 copia = 1
-                indiv = Individuo()
                 indiv = poblacion[i]
                 pob_salida.append(indiv)
+
             copias[i] = copias[i] + copia
             if sum(copias) == len(poblacion):
                 break
-    print('copias' + str(copias))
+    # print('copias' + str(copias))
     return pob_salida
 
 def seleccion(poblacion,preservar,porcentaje):
+
+
+    if porcentaje == 0:
+        return []
     cant = int(len(poblacion)*porcentaje)
+
+    pob_elitista = []
     if cant > preservar:
-        pob_elitista = poblacion[0:preservar]
+        for i in range(preservar):
+            pob_elitista.append(poblacion[i])
         pob_controlada = seleccionControlada(poblacion[preservar:cant])
         pob_salida = pob_elitista + pob_controlada
-        ordenarPoblacion(pob_salida,'fitness')
         return pob_salida
     elif cant == preservar:
         pob_salida = poblacion[0:preservar]
-        ordenarPoblacion(pob_salida, 'fitness')
         return pob_salida
     else:
-        print('La cantidad a preservar debe ser menor o igual a la cantidad total a seleccionar')
-
-
-def cruzaUnPunto(ind1, ind2):
-    # print(ind1.valor)
-    # print(ind2.valor)
-    size = len(ind1.valor)
-    cxpoint = random.randint(1, size - 1)
-    valor1 = ind1.valor[:cxpoint] + ind2.valor[cxpoint:]
-    valor2 = ind2.valor[:cxpoint] + ind1.valor[cxpoint:]
-    ind1.setValor(valor1,dataset)
-    ind2.setValor(valor2,dataset)
-    # ind1.valor[cxpoint:], ind2.valor[cxpoint:] = ind2.valor[cxpoint:], ind1.valor[cxpoint:]
-    # print(ind1.valor)
-    # print(ind2.valor)
-
-
-    return ind1, ind2
+        warnings.warn_explicit('La cantidad a preservar debe ser menor o igual a la cantidad total a seleccionar')
 
 def ordenarPoblacion(poblacion, atr):
     if atr == 'fitness':
         poblacion.sort(key=lambda individuo: individuo.fitness, reverse=True)
     if atr == 'copias':
         poblacion.sort(key=lambda individuo: individuo.copias, reverse=True)
+    if atr == 'valor':
+        poblacion.sort(key=lambda individuo: individuo.valor, reverse=True)
 
 def evaluarFitness(poblacion):
     for ind in poblacion:
         ind.setFitness()
 
-    ordenarPoblacion(poblacion, 'fitness')
 
     # for ind in poblacion:
     #     ind.corregirFitness()
 
-def cruzaPoblacion(pob):
-    poblacion = pob
-    while True:
-        ordenarPoblacion(poblacion,'copias')
+def cruzaPoblacion(poblacion, porcentaje):
+    #arreglar, que si la cantidad a cruzar es 4, no busque solo en [0,1,2,3] sino en toda la poblacion [0,...,len(pob)]
+    '''Elegir siempre un porcentaje que de una cantidad par de individuos'''
+    # poblacion = copy(pob)
+    pob_salida = []
+    cant_indiv = int(len(poblacion) * porcentaje)
+    indices = lista_de_enteros(0, len(poblacion))
+    lt = []
+    for i in range(cant_indiv):
+        x = random.choice(indices)
+        indices = restar_listas(indices, [x])
+        lt = lt + [x]
 
-        if poblacion[0].copias != 0:
-            cruzaUnPunto(poblacion[0],poblacion[1])
-            poblacion[0].copias = poblacion[0].copias - 1
-            poblacion[1].copias = poblacion[1].copias - 1
-        else:
+    while True:
+        rand1 = random.choice(lt)
+        lt = restar_listas(lt, [rand1])
+        rand2 = random.choice(lt)
+        lt = restar_listas(lt, [rand2])
+        ind1 = copy(poblacion[rand1])
+        ind2 = copy(poblacion[rand2])
+        cruzaUnPunto(ind1,ind2)
+        pob_salida.append(ind1)
+        pob_salida.append(ind2)
+        if not lt:
             break
-    return poblacion
+
+    return pob_salida
+
+
+
+def cruzaUnPunto(i1, i2):
+    '''Cruza simple con punto de cruza aleatorio'''
+    # print(ind1.valor)
+    # print(ind2.valor)
+    ind1 = i1
+    ind2 = i2
+    size = len(ind1.valor)
+    cxpoint = random.randrange(1, size)
+    # print('punto de cruza: '+ str(cxpoint) )
+    valor1 = ind1.valor[:cxpoint] + ind2.valor[cxpoint:]
+    valor2 = ind2.valor[:cxpoint] + ind1.valor[cxpoint:]
+    ind1.setValor(valor1)
+    ind2.setValor(valor2)
+    # ind1.valor[cxpoint:], ind2.valor[cxpoint:] = ind2.valor[cxpoint:], ind1.valor[cxpoint:]
+    # print(ind1.valor)
+    # print(ind2.valor)
+    return ind1, ind2
 
 def imprimirPoblacion(pob):
     # copias = 0
     ordenarPoblacion(pob,'fitness')
     for i in range(len(pob)):
-        print('Fitness: '+ str(pob[i].fitness))
+        print('Valor: '+str(pob[i].valor) + ' Fitness: '+ str(pob[i].fitness))
         # copias = copias + pob[i].copias
     print('Cant individuos: ' + str(len(pob)))
     # print('Cant copias: ' + str(copias))
+
+def lista_de_enteros(a, b):
+    lt = []
+    for i in range(a, b ):
+        lt = lt + [i]
+    return lt
+
+def restar_listas(lt1, lt2):
+    a = lt1
+    b = lt2
+    a = [item for item in a if item not in b]
+    return a
 
 
 dataset = leerTxt("C:\\Franco\\Facultad\\IA\\dataset01.txt")
@@ -242,39 +280,60 @@ maxy = round(max(y))
 minx = round(min(x))
 miny = round(min(y))
 cantidadIndividuos = 10
-tamanoIndividuo = 3
-cantPreservar = 0
-iteraciones = 1
+tamanoIndividuo = 2
+iteraciones = 1000
+cantPreservar = 1
+porc_seleccion = 0.2
+porc_cruza = 0.8
+porc_mutacion = 0
 
-
-
+#
+# pob_anterior = []
+# pob_siguiente = []
+#
+# pob_anterior  = generarPoblacionInicial(cantidadIndividuos,tamanoIndividuo,minx,maxx,miny,maxy,dataset)
+# evaluarFitness(pob_anterior)
+# print('Poblacion anterior')
+# imprimirPoblacion(pob_anterior)
+#
+#
+#
+# print()
+#
+# for i in range(iteraciones):
+#     pob_siguiente = seleccion(pob_anterior,cantPreservar,porc_seleccion)
+#     pob_siguiente = pob_siguiente + cruzaPoblacion(pob_anterior,porc_cruza)
+#     evaluarFitness(pob_siguiente)
+#
+#     pob_anterior = pob_siguiente
+#
+#
+#
+#
+#
+# print()
+# print('pob sig')
+# imprimirPoblacion(pob_siguiente)
 
 
 pob_anterior  = generarPoblacionInicial(cantidadIndividuos,tamanoIndividuo,minx,maxx,miny,maxy,dataset)
-pob_siguiente = []
-
 evaluarFitness(pob_anterior)
 imprimirPoblacion(pob_anterior)
+ind_viejo = copy(pob_anterior[0])
+print(ind_viejo.fitness)
 
 for i in range(iteraciones):
-    pob_siguiente = seleccion(pob_anterior,0,1)
+    pob_sig = seleccion(pob_anterior,1,0.2) + cruzaPoblacion(pob_anterior,porc_cruza)
+    evaluarFitness(pob_sig)
+    pob_anterior = pob_sig
 
-print()
-imprimirPoblacion(pob_siguiente)
 
 
-# p = ''
-# f = ''
-#
-#
-# for ind in poblacion:
-#     p = p + str(ind.valor)
-#     f = f + str(ind.fitness) + '/'
-# print(p)
-# print(f)
-# print(poblacion[0].valor)
-# print(poblacion[0].fitness)
-#
+imprimirPoblacion(pob_sig)
+ind_nuevo = pob_sig[0]
+print(ind_nuevo.fitness)
+
+
 
 
 
@@ -327,38 +386,39 @@ imprimirPoblacion(pob_siguiente)
 
 #------------------------------ Graficos
 
-# individuo2 = pob_siguiente[0]
-# individuo1 = pob_siguiente[len(pob_siguiente)-1]
-#
-# fig1 = pl.figure()
-# fig2 = pl.figure()
-# ax1 = fig1.add_subplot(111)
-# ax1.set_title('Peor individuo, Fitness: '+str(individuo1.fitness)+' -'+' Individuos: '+str(cantidadIndividuos))
-#
-# ax2 = fig2.add_subplot(111)
-# ax2.set_title('Mejor individuo, Fitness: '+str(individuo2.fitness)+' -'+' Individuos: '+str(cantidadIndividuos))
-#
-#
-#
-# for i in range(len(individuo1.puntos)):
-#     if individuo1.puntos[i]:
-#         px,py = zip(*individuo1.puntos[i])
-#         ax1.scatter(px,py)
-#
-#
-# for centroide in individuo1.valor:
-#     px,py = centroide
-#     ax1.scatter(px,py)
-#
-#
-# for i in range(len(individuo2.puntos)):
-#     if individuo2.puntos[i]:
-#         px,py = zip(*individuo2.puntos[i])
-#         ax2.scatter(px,py)
-#
-#
-# for centroide in individuo2.valor:
-#     px,py = centroide
-#     ax2.scatter(px,py)
-#
-# pl.show()
+
+individuo1 = ind_nuevo
+individuo2 = ind_viejo
+
+fig1 = pl.figure()
+fig2 = pl.figure()
+ax1 = fig1.add_subplot(111)
+ax1.set_title('Nuevo individuo, Fitness: '+str(individuo1.fitness)+' -'+' Individuos: '+str(cantidadIndividuos))
+
+ax2 = fig2.add_subplot(111)
+ax2.set_title('Viejo individuo, Fitness: '+str(individuo2.fitness)+' -'+' Individuos: '+str(cantidadIndividuos))
+
+
+
+for i in range(len(individuo1.puntos)):
+    if individuo1.puntos[i]:
+        px,py = zip(*individuo1.puntos[i])
+        ax1.scatter(px,py)
+
+
+for centroide in individuo1.valor:
+    px,py = centroide
+    ax1.scatter(px,py)
+
+
+for i in range(len(individuo2.puntos)):
+    if individuo2.puntos[i]:
+        px,py = zip(*individuo2.puntos[i])
+        ax2.scatter(px,py)
+
+
+for centroide in individuo2.valor:
+    px,py = centroide
+    ax2.scatter(px,py)
+
+pl.show()
